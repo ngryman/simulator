c9e13a56dbc7140b93c744d54f5a348026b8ebd4
ngryman <ngryman@gmail.com>
2015-07-19 15:45:32 +0200

    yo

--- test/lib/flavours.js
+++ test/lib/flavours.js
@@ -3,10 +3,10 @@
 var test = require('./test')
 
 module.exports = function flavours(callback) {
-  if ('Promise' in window) {
-    test.promise = true
-    context('with promise flavour', callback)
-  }
+  // if ('Promise' in window) {
+  //   test.promise = true
+  //   context('with promise flavour', callback)
+  // }
   test.promise = false
   context('with callback flavour', callback)
 }
