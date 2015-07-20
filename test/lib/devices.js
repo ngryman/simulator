ab2c4782d4d9af75c41952001329a4a1a3e5b4f9
ngryman <ngryman@gmail.com>
2015-07-20 13:17:01 +0200

    adapted tap test

---
+++ test/lib/devices.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var Simulator = require('Simulator')
+
+module.exports = function devices(callback) {
+  Object.getOwnPropertyNames(Simulator.devices).forEach(function(name) {
+    context('given ' + name + ' device', callback.bind(null, name, Simulator.device.eventsMap))
+  })
+}
--- test/simulator.tap.js
+++ test/simulator.tap.js
@@ -4,33 +4,35 @@ var flavours = require('./lib').flavours
   , test = require('./lib').test
 
 describe('Simulator.tap', function() {
-  flavours(function(usePromise) {
-    it('triggers mousedown', test(Simulator.tap, function(spies) {
-      expect(spies.down).to.have.been.called.and.calledOn(el)
-    }, {}, usePromise))
+  devices(function(name, events) {
+    flavours(function(usePromise) {
+      it('triggers ' + events.down, test(Simulator.tap, function(spies) {
+        expect(spies.down).to.have.been.called.and.calledOn(el)
+      }, {}, usePromise))
 
-    it('triggers mouseup', test(Simulator.tap, function(spies) {
-      expect(spies.up).to.have.been.called.and.calledOn(el)
-    }, {}, usePromise))
+      it('triggers ' + events.up, test(Simulator.tap, function(spies) {
+        expect(spies.up).to.have.been.called.and.calledOn(el)
+      }, {}, usePromise))
 
-    it('triggers mouseup after mousedown', test(Simulator.tap, function(spies) {
-      expect(spies.down).to.have.been.called.and.calledOn(el)
-      expect(spies.up).to.have.been.called.and.calledOn(el)
-      expect(spies.down).to.have.been.calledBefore(spies.up)
-    }, {}, usePromise))
+      it('triggers ' + events.up + ' after ' + events.down, test(Simulator.tap, function(spies) {
+        expect(spies.down).to.have.been.called.and.calledOn(el)
+        expect(spies.up).to.have.been.called.and.calledOn(el)
+        expect(spies.down).to.have.been.calledBefore(spies.up)
+      }, {}, usePromise))
 
-    it('can delay mouseup event', test(Simulator.tap, function(spies, timers) {
-      expect(Date.now() - timers.before).to.be.at.least(50)
-      expect(spies.up).to.have.been.called.and.calledOn(el)
-    }, { duration: 50 }, usePromise))
+      it('can delay ' + events.up + ' event', test(Simulator.tap, function(spies, timers) {
+        expect(Date.now() - timers.before).to.be.at.least(50)
+        expect(spies.up).to.have.been.called.and.calledOn(el)
+      }, { duration: 50 }, usePromise))
 
-    it('can trigger at specific coordinates', test(function() {
-      return Simulator.position(10, 10).tap.apply(null, arguments)
-    }, function(spies) {
-      expect(spies.down.args[0][0].screenX).to.equal(10)
-      expect(spies.down.args[0][0].screenY).to.equal(10)
-      expect(spies.down.args[0][0].clientX).to.equal(10)
-      expect(spies.down.args[0][0].clientY).to.equal(10)
-    }, {}, usePromise))
+      it('can trigger at specific coordinates', test(function() {
+        return Simulator.position(10, 10).tap.apply(null, arguments)
+      }, function(spies) {
+        expect(spies.down.args[0][0].screenX).to.equal(10)
+        expect(spies.down.args[0][0].screenY).to.equal(10)
+        expect(spies.down.args[0][0].clientX).to.equal(10)
+        expect(spies.down.args[0][0].clientY).to.equal(10)
+      }, {}, usePromise))
+    })
   })
 })
